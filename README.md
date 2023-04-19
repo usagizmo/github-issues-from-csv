b86897d52747865c1437bbf12c9cd9bf6afe11e8
usagizmo <usagizmo@gmail.com>
2023-04-20 06:29:13 +0900

    feat: first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# GitHub issues from CSV
+
+This is a Node.js script that allows you to add issues to a GitHub repository from a CSV file. It uses the csv-parser library to read the CSV file and the octokit library to interact with the GitHub API.
+
+## Prerequisites
+
+Before running the script, you need to have:
+
+- A GitHub account
+- A personal access token with the repo scope
+- Node.js installed on your machine
+
+## Installation
+
+1. Clone this repository to your local machine
+2. Navigate to the cloned directory and run npm install to install the dependencies
+3. Create a `.env` file from `.env.example` and set the following environment variables:
+
+```.env
+TOKEN=YOUR_GITHUB_PERSONAL_ACCESS_TOKEN
+OWNER=GITHUB_REPO_OWNER
+```
+
+## Usage
+
+To use the script:
+
+1. Run `npm run add-issues` from the command line
+2. Enter the name of the repository you want to add issues to
+3. Choose the CSV file you want to use (either `sample-1.csv` or `sample-2.csv`)
+4. The script will create issues in the repository based on the data in the CSV file
+
+## License
+
+This project is licensed under the MIT License.
---
+++ src/add-issues.js
@@ -0,0 +1,86 @@
+import fs from 'fs';
+import csvParser from 'csv-parser';
+import readline from 'readline';
+import { Octokit } from 'octokit';
+
+const TOKEN = process.env.TOKEN;
+const OWNER = process.env.OWNER;
+
+const readCSV = (filePath) => {
+  return new Promise((resolve, reject) => {
+    const issueList = [];
+
+    fs.createReadStream(filePath)
+      .pipe(csvParser())
+      .on('data', (row) => {
+        issueList.push({ title: row.title, body: row.body });
+      })
+      .on('end', () => {
+        resolve(issueList);
+      })
+      .on('error', (err) => {
+        reject(err);
+      });
+  });
+};
+
+const createIssues = async (issues, octokit, repo) => {
+  for (const issue of issues) {
+    try {
+      await octokit.rest.issues.create({
+        owner: OWNER,
+        repo,
+        title: issue.title,
+        body: issue.body,
+      });
+      console.log(`Issue created: ${issue.title}`);
+    } catch (error) {
+      console.error(`Error creating issue: ${issue.title} - ${error.message}`);
+    }
+  }
+};
+
+const askQuestions = async () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const question = (query) => {
+    return new Promise((resolve) => {
+      rl.question(query, (answer) => {
+        resolve(answer);
+      });
+    });
+  };
+
+  const repo = await question('Enter the GitHub repository name: ');
+  const csvSelection = await question('\n0: sample-1.csv\n1: sample-2.csv\nChoose the CSV file: ');
+
+  let csvFilePath = '';
+
+  switch (csvSelection.toLowerCase()) {
+    case '0':
+      csvFilePath = './src/data/sample-1.csv';
+      break;
+    case '1':
+      csvFilePath = './src/data/sample-2.csv';
+      break;
+    default:
+      console.error('Invalid selection. Please choose 0 or 1.');
+      process.exit(1);
+  }
+
+  rl.close();
+
+  return { repo, csvFilePath };
+};
+
+(async () => {
+  const { repo, csvFilePath } = await askQuestions();
+  const issues = await readCSV(csvFilePath);
+  const octokit = new Octokit({ auth: TOKEN });
+
+  await createIssues(issues, octokit, repo);
+  console.log('All issues have been created');
+})();
